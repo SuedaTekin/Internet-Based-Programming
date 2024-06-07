449fdbbb06c2dd65b27d425d14a728fa5ec9156f
Sueda Tekin <115072432+SuedaTekin@users.noreply.github.com>
2024-06-07 22:19:04 +0300

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,36 @@
+<?php
+
+$db_host="localhost";
+$db_user="root";
+$db_password="";
+$database="hobby";
+
+$conn = new mysqli($db_host, $db_user, $db_password, $database);
+
+
+$sql = "CREATE DATABASE IF NOT EXISTS $database";
+if ($conn->query($sql) === false) {
+    die("Error creating database: " . $conn->error);
+}
+
+$conn->select_db($database);
+
+$sql = "CREATE TABLE IF NOT EXISTS messages (
+        id INT(11) AUTO_INCREMENT PRIMARY KEY,
+        f_name VARCHAR(40) NOT NULL,
+        l_name VARCHAR(40) NOT NULL,
+        email VARCHAR(40) NOT NULL,
+        topic VARCHAR(20) NOT NULL,
+        message VARCHAR(250) NOT NULL,
+        message_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)";
+
+if ($conn->query($sql) === false) {
+    die("Error creating table: " . $conn->error);
+}
+?>
+
+
+
+
+
